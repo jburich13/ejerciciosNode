4e892bd973217807d4d36fe283cfc2ef68c058f0
Juan Burich <burichojrs@gmail.com>
2021-06-15 13:46:20 -0300

    Ejercicio 3

--- Clase3/index.js
+++ Clase3/index.js
@@ -1,10 +1,18 @@
-async function imprimirPalabras(texto = "", tiempo) {
+async function imprimirPalabras(texto = "", tiempo = 1000) {
     const promise = await new Promise((resolve, reject) => {
+        let i = 0;
         let texto2 = texto.split(" ")
-        for (let i = 0; i < texto2.length; i++) {
-            setTimeout(() => { console.log(texto2[i]) }, 10000)
-        }
+        const intervalo = setInterval(() => {
+            console.log(texto2[i])
+            i++
+            if (texto2.length == i) {
+                clearInterval(intervalo)
+            }
+        }, tiempo)
     })
     return promise
 }
-imprimirPalabras("Hola como estas", 5000)
+
+imprimirPalabras("Hola como estas")
+imprimirPalabras("Juan me llamo yo", 6000)
+imprimirPalabras("Haciendo el curso de coder", 18000)
