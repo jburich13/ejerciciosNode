90f90615e9274bf039c610a75e8a5b4b5bb2b447
Juan Burich <burichojrs@gmail.com>
2021-07-12 11:49:45 -0300

    Ejercicio clase 9 en proceso

---
+++ Clase9/index.js
@@ -0,0 +1,74 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+const port = 8080
+const routerProductos = express.Router()
+let acID=0
+
+function getRandom(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+let productos = [
+    {
+        title:"Producto"+getRandom(0,100),
+        price:"Precio: $"+getRandom(0,1001),
+        thumbnail:"Img",
+        id:0},
+    {
+        title:"Producto"+getRandom(0,100),
+        price:"Precio: $"+getRandom(0,1001),
+        thumbnail:"Img",
+        id:1
+    }, {
+        title:"Producto"+getRandom(0,100),
+        price:"Precio: $"+getRandom(0,1001),
+        thumbnail:"Img",
+        id:2
+    }]
+
+const productoAGuardar = {
+    title:"Producto"+getRandom(0,100),
+    price:"Precio: $"+getRandom(0,1001),
+    thumbnail:"Img",
+    id:3
+}
+
+routerProductos.get("/listar",(req,res)=>{
+    if(productos.length===0){
+        res.json({error:"No hay productos cargados"})
+    } else res.json(productos)
+
+})
+
+routerProductos.get("/listar/:id",(req,res)=>{
+    productos.forEach(element =>{
+        if(element.id ===req.params.id){
+            res.json(productos[req.params.id])
+        } else res.json({error:"Producto no encontrado"})
+    })
+
+})
+routerProductos.post("/guardar",(req,res)=>{
+    productos.push(productoAGuardar)
+    res.json(productoAGuardar)
+})
+
+routerProductos.delete('/borrar/:id', (req, res) => {
+    productos = productos.filter((producto) => producto.id != req.params.id)
+    res.json(productos)
+})
+
+routerProductos.put('/actualizar/:id', (req, res) => {
+    for (let i=0; i < productos.length; i++) {
+        console.log(`for ${i}`)
+        if (productos[i].id == req.params.id) {
+            productos[i] = productoAGuardar
+        }
+    }
+    res.send(productos)
+})
+app.use('/api/productos', routerProductos)
+
+app.listen(port,()=>console.log("Escuchando puerto "+port))
