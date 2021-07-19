5d1b3032785114cd6dd069f13437611dcdb5392c
Juan Burich <burichojrs@gmail.com>
2021-07-19 12:10:00 -0300

    subido ejercicio 10

---
+++ Clase10/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const hlbs =  require("express-handlebars")
+const port = 8080
+
+const app = express()
+
+app.engine('handlebars', hlbs());
+app.set("view engine", "handlebars");
+
+
+app.get("/",(req,res)=>{
+    res.render("home",)
+})
+
+app.listen(port,()=>console.log("Escuchando puerto "+port))
